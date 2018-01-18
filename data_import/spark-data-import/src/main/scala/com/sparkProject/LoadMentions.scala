c3c7570b3038a6ff14097bc2f1ba192048f79d9c
Cyril Nguyen <cyril.nguyen@telecom-paristech.fr>
2018-01-18 16:29:34 +0100

    Update the data import spark project
    The project is functional
    A documentation has been added (readme.md in the project directory)

--- data_import/spark-data-import/src/main/scala/com/sparkProject/LoadMentions.scala
+++ data_import/spark-data-import/src/main/scala/com/sparkProject/LoadMentions.scala
@@ -1,4 +1,3 @@
-
 package com.sparkProject
 
 import org.apache.spark.SparkConf
@@ -6,15 +5,25 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 
 import java.io.{BufferedReader, InputStreamReader}
+import java.sql.Timestamp
 import java.util.zip.ZipInputStream
 import org.apache.spark.input.PortableDataStream
 import com.mongodb.spark._
 import com.mongodb.spark.config._
 import org.bson.Document
 
-object DataImport {
+object LoadMentions {
 
   def main(args: Array[String]): Unit = {
+
+    var inputFileDateFormat = "*"
+    if (args.length > 0)
+      inputFileDateFormat = args(0)
+
+    var schema = "year"
+    if (args.length > 1)
+      schema = args(1)
+    val mongoOutputUri = "mongodb://ip-172-31-8-31.ec2.internal/" + schema + ".test2"
 
     val conf = new SparkConf().setAll(Map(
       "spark.scheduler.mode" -> "FIFO",
@@ -23,7 +32,7 @@ object DataImport {
       "spark.serializer" -> "org.apache.spark.serializer.KryoSerializer",
       "spark.kryoserializer.buffer.max" -> "1g",
       "spark.shuffle.file.buffer" -> "32k",
-      "spark.mongodb.output.uri" -> "mongodb://ip-172-31-8-31.ec2.internal/test_CNG.test2"
+      "spark.mongodb.output.uri" -> mongoOutputUri
     ))
 
     val spark = SparkSession
@@ -36,9 +45,14 @@ object DataImport {
 
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", "AKIAJJTGBQ7SKKM2B7SQ") // mettre votre ID du fichier credentials.csv
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", "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") // mettre votre secret du fichier credentials.csv
-    spark.sparkContext.hadoopConfiguration.setInt("fs.s3a.connection.maximum", 500)
+    spark.sparkContext.hadoopConfiguration.setInt("fs.s3a.connection.maximum", 50000)
 
-    val zipRDD = spark.sparkContext.binaryFiles("s3a://gdelt-sources/201701*.export.CSV.zip")
+    val mediadf = spark.read.format("csv").option("header", true).
+      load("s3a://gdelt-sources/media.csv").
+      withColumnRenamed("mentionsourcename", "sourceName")
+
+    val zipRDD = spark.sparkContext.binaryFiles("s3a://gdelt-sources/" + inputFileDateFormat + ".mentions.CSV.zip")
+    // val zipRDD = spark.sparkContext.binaryFiles("file:///home/cyril/Data/INF728/Projet/20170108053000.mentions.CSV.zip")
 
     val textRDD = zipRDD.flatMap {
       case (name: String, content: PortableDataStream) =>
@@ -51,19 +65,22 @@ object DataImport {
           }
     }
 
-    textRDD.count()
-
     val df = textRDD.map(line => line.split("\t")).toDF.
       withColumn("GlobalEventID", col("value").getItem(0)).
-      withColumn("Actor1Code", col("value").getItem(5)).
-      withColumn("Actor1Name", col("value").getItem(6)).
-      withColumn("Day", col("value").getItem(1)).
-      withColumn("Actor2Code", col("value").getItem(15)).
-      withColumn("Actor2CountryCode", col("value").getItem(17)).
-      drop("value")
+      withColumn("MentionTimeDate", col("value").getItem(2)).
+      withColumn("MentionType", col("value").getItem(3).cast("Int")).
+      withColumn("MentionSourceName", col("value").getItem(4)).
+      withColumn("MentionIdentifier", col("value").getItem(5)).
+      withColumn("MentionDocTone", col("value").getItem(13).cast("Int")).
+      drop("value").
+      filter(col("MentionType") === 1).
+      withColumn("MentionDate", to_timestamp(substring(col("MentionTimeDate"), 0, 8), "yyyyMMdd")).
+      drop("MentionTimeDate", "MentionType").
+      join(mediadf, col("MentionSourceName") === mediadf("sourceName")).
+      drop("MentionSourceName", "sourceName", "CountryHumanName").
+      withColumnRenamed("FIPSCountryCode", "CountryCode")
 
-    MongoSpark.save(df.write.option("collection", "test3").mode("append"))
+    MongoSpark.save(df.write.option("collection", "mentions").mode("append"))
 
   }
-
 }
